d393d5854b4fc9eeb5a35896a4bdbf577ade05af
fcoeliesio <fcoeliesio@gmail.com>
2024-01-28 21:29:35 -0300

    Commit 2 - Replace Temp with Query

--- index.js
+++ index.js
@@ -8,15 +8,20 @@ function gerarFaturaStr (fatura, pecas) {
                           { style: "currency", currency: "BRL",
                             minimumFractionDigits: 2 }).format;
 
+    // funcção query
+    function getPeca(apresentacao){
+      return pecas[apresentacao.id];
+    }
      
     for (let apre of fatura.apresentacoes) {
-      const peca = pecas[apre.id];
+      //const peca = pecas[apre.id];
+      const peca = getPeca(apre);
       let total = 0;
     
     // Função extraída 
-    function calcularTotalApresentacao(apre, peca){
+    function calcularTotalApresentacao(apre){
       let total = 0;
-      switch (peca.tipo) {
+      switch (getPeca(apre).tipo) {
       case "tragedia":
         total = 40000;
         if (apre.audiencia > 30) {
@@ -38,11 +43,11 @@ function gerarFaturaStr (fatura, pecas) {
 
       // créditos para próximas contratações
       creditos += Math.max(apre.audiencia - 30, 0);
-      if (peca.tipo === "comedia") 
+      if (getPeca(apre).tipo === "comedia") 
          creditos += Math.floor(apre.audiencia / 5);
   
       // mais uma linha da fatura
-      faturaStr += `  ${peca.nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+      faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
       totalFatura += total;
     }
 
