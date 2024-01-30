a86f46f578aa3c7db7e1a4902c226703c3d89515
gusta <gustavo.aragao@aluno.ufca.edu.br>
2024-01-29 23:58:08 -0300

    Commit 9 - Criando Arquivos

---
+++ apresentacao.js
@@ -0,0 +1,13 @@
+var formatarMoeda = require("./util.js");
+
+module.exports = function gerarFaturaStr(fatura, calc) {
+    let faturaStr = `Fatura ${fatura.cliente}\n`;
+    for (let apre of fatura.apresentacoes) {
+        faturaStr += `  ${calc.repo.getPeca(apre).nome}: ${formatarMoeda(
+            calc.calcularTotalApresentacao(apre)
+        )} (${apre.audiencia} assentos)\n`;
+    }
+    faturaStr += `Valor total: ${formatarMoeda(calc.calcularTotalFatura(fatura.apresentacoes))}\n`;
+    faturaStr += `Créditos acumulados: ${calc.calcularTotalCreditos(fatura.apresentacoes)}\n`;
+    return faturaStr;
+}
