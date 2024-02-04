7bf2c92feb047a7d980dc1927b6dbf60c1bdfe17
mfjacome <matheus.fause@gmail.com>
2024-02-04 18:06:08 -0300

    Commit 2 - Replace Temp with Query

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const { readFileSync } = require('fs');
 
 function gerarFaturaStr(fatura, pecas) {
+
+    // função query
+    function getPeca(apresentacao){
+      return pecas[apresentacao.id];
+    }
+
     let totalFatura = 0;
     let creditos = 0;
     let faturaStr = `Fatura ${fatura.cliente}\n`;
     const formato = new Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL", minimumFractionDigits: 2 }).format;
 
     // Função extraída
-    function calcularTotalApresentacao(apre, peca) {
+    function calcularTotalApresentacao(apre) {
         let total = 0;
-        switch (peca.tipo) {
+        switch (getPeca(apre).tipo) {
             case "tragedia":
                 total = 40000;
                 if (apre.audiencia > 30) {
@@ -30,8 +36,9 @@ function gerarFaturaStr(fatura, pecas) {
     }
 
     for (let apre of fatura.apresentacoes) {
-        const peca = pecas[apre.id];
-        let total = calcularTotalApresentacao(apre, peca);
+        //const peca = pecas[apre.id];
+        const peca = getPeca(apre);
+        let total = calcularTotalApresentacao(apre);
 
         // Créditos para próximas contratações
         creditos += Math.max(apre.audiencia - 30, 0);
